35b3d8e85361d00f0f681bb756c2f1f7bddc22e4
brych125 <brych.126@gmail.com>
2024-11-21 01:20:41 +0200

    Start to use embedded healthcheck

--- MyWebAPI/Program.cs
+++ MyWebAPI/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +16,8 @@ builder.Services.AddStackExchangeRedisCache(options =>
 });
 
 var app = builder.Build();
+
+app.MapHealthChecks("/healthz");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
